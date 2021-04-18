db7f4905c14946b15015b7ce9490f4b3d169453d
Okan Aslan <okanaslan.08@gmail.com>
2021-04-18 13:50:47 +0300

    Better tests and GitHub actions.

--- test/controllers/user.test.ts
+++ test/controllers/user.test.ts
@@ -1,7 +1,37 @@
 import { server } from "../../src/server";
 import supertest from "supertest";
+import { Database } from "../../src/database";
+import { Connection } from "mongoose";
+import { config } from "dotenv";
 
-test("Get /user", async () => {
-    const response = await supertest(server).get("/user").set("Accept", "application/json");
-    expect(response.body.username).toBe("okanaslan");
+describe("# User", () => {
+    let mongo: Connection;
+
+    beforeAll(async () => {
+        const env = config();
+        if (env.error == null && process.env.MongoURI != null) {
+            try {
+                mongo = await Database.connect(process.env.MongoURI!);
+            } catch (error) {
+                fail(error);
+            }
+        } else {
+            fail("Environment Error");
+        }
+    });
+
+    afterAll(async () => {
+        try {
+            await mongo.close();
+        } catch (error) {
+            fail(error);
+        }
+    });
+
+    describe("Get /user", () => {
+        test("Should return a user.", async () => {
+            const response = await supertest(server).get("/user").auth("", { type: "bearer" }).set("Accept", "application/json");
+            expect(response.body.username).toBe("okanaslan");
+        });
+    });
 });
