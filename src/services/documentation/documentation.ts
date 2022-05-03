6712f973aea51583851d0138cd38201ed518f6db
Okan Aslan <okanaslan.08@gmail.com>
2022-05-03 22:10:09 +0300

    Better documentation abstraction.

--- src/services/documentation/documentation.ts
+++ src/services/documentation/documentation.ts
@@ -1,53 +1,17 @@
 import { Express } from "express";
-import { OpenApiBuilder, SchemaObject } from "openapi3-ts";
-import { SwaggerUIExpress } from "./external/swaggerUIExpress";
-
-import { TSJsonSchemaGenerator } from "./external/tsJsonSchemaGenerator";
-
+import { OpenAPI3 } from "./external/openAPI3";
 import { DocumentationInput } from "./types/DocumentationInput";
-import { DocumentationOutput } from "./types/DocumentationOutput";
 
 export class Documentation {
-    static openAPI = new OpenApiBuilder({
-        openapi: "3.0.0",
-        info: {
-            title: "API",
-            summary: "Core Backend API",
-            version: "1.0.0",
-            contact: {
-                name: "Okan Aslan",
-            },
-        },
-        servers: [{ url: "http://localhost:{port}", description: "Local", variables: { port: { default: "3000" } } }],
-        paths: {},
-        tags: [{ name: "user", description: "User related endpoints" }],
-        components: { schemas: {} },
-    });
-
-    private static addToSchemas(schemas: { [key: string]: SchemaObject }) {
-        for (const key of Object.keys(schemas)) {
-            const additionalSchema = schemas[key];
-            if (additionalSchema != null) {
-                Documentation.openAPI.addSchema(key, additionalSchema);
-            }
-        }
-    }
-
-    private static addToPath(doc: DocumentationOutput) {
-        Documentation.openAPI.addPath(doc.endpoint, doc.doc);
-    }
-
     static add(input: DocumentationInput) {
-        const doc = TSJsonSchemaGenerator.generateDocumentation(input);
-        Documentation.addToPath(doc);
-        Documentation.addToSchemas(doc.additionalSchemas);
+        OpenAPI3.add(input);
     }
 
     static get() {
-        return Documentation.openAPI.getSpec();
+        return OpenAPI3.get();
     }
 
     static serve(expressServer: Express) {
-        return SwaggerUIExpress.serve(expressServer);
+        OpenAPI3.serve(expressServer);
     }
 }
---
+++ src/services/documentation/external/openAPI3.ts
@@ -0,0 +1,58 @@
+import { Express } from "express";
+import { OpenApiBuilder, SchemaObject } from "openapi3-ts";
+import { SwaggerUIExpress } from "../external/swaggerUIExpress";
+
+import { TSJsonSchemaGenerator } from "../external/tsJsonSchemaGenerator";
+
+import { DocumentationInput } from "../types/DocumentationInput";
+import { DocumentationOutput } from "../types/DocumentationOutput";
+
+export class OpenAPI3 {
+    static openAPI = new OpenApiBuilder({
+        openapi: "3.0.0",
+        info: {
+            title: "API",
+            summary: "Core Backend API",
+            version: "1.0.0",
+            contact: {
+                name: "Okan Aslan",
+            },
+        },
+        servers: [{ url: "http://localhost:{port}", description: "Local", variables: { port: { default: "3000" } } }],
+        paths: {},
+        tags: [{ name: "user", description: "User related endpoints" }],
+        components: { schemas: {} },
+    });
+
+    private static addToSchemas(schemas: { [key: string]: SchemaObject }) {
+        for (const key of Object.keys(schemas)) {
+            const additionalSchema = schemas[key];
+            if (additionalSchema != null) {
+                OpenAPI3.openAPI.addSchema(key, additionalSchema);
+            }
+        }
+    }
+
+    private static addToPath(doc: DocumentationOutput) {
+        OpenAPI3.openAPI.addPath(doc.endpoint, doc.doc);
+    }
+
+    static add(input: DocumentationInput) {
+        const doc = TSJsonSchemaGenerator.generateDocumentation(input);
+        OpenAPI3.addToPath(doc);
+        OpenAPI3.addToSchemas(doc.additionalSchemas);
+    }
+
+    static get() {
+        const doc = OpenAPI3.openAPI.getSpec();
+        if (doc.components?.schemas != null) {
+            doc.definitions = doc.components.schemas;
+            doc.components.schemas = undefined;
+        }
+        return doc;
+    }
+
+    static serve(expressServer: Express) {
+        SwaggerUIExpress.serve(expressServer);
+    }
+}
